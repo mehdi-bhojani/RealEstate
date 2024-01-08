f8eea570d823bf512388114f18ebdb086ba769f6
mehdi-bhojani <m.mehdibhojani1@gmail.com>
2024-01-08 14:45:33 +0500

    creating pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signout from './pages/Signout';
+import Home from './pages/Home';
+import './assets/main.css';
+
 
 export default function App() {
   return (
-    <h1 className='text-purple-900'>Apps</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signin />}/>
+        <Route path="/sign-out" element={<Signout />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <p className='text-3xl font-bold underline'>Home</p>
+    </>
+  )
+}
