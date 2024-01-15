b8c652d21ce2500ad8c66efe5babb511ad9fb256
mehdi-bhojani <m.mehdibhojani1@gmail.com>
2024-01-15 15:45:46 +0500

    Connect to databse

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
+//importing libraries
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+
+// initialization of imported library
+dotenv.config();
 const app = express();
 
-const port = 3000
+//conneting MongoDB
+mongoose.connect(process.env.MONGO)
+.then(()=>{console.log("Monoose conneted")})
+.catch((err)=>{console.log(err)})
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+
+//listening app at port 3000
+const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
