af1402fca485fb336d9a1a3d52b787d1bf4c39ff
mehdi-bhojani <m.mehdibhojani1@gmail.com>
2024-01-20 03:02:52 +0500

    create sigup form functionality

--- api/index.js
+++ api/index.js
@@ -1,40 +1,41 @@
-//importing libraries
+// importing libraries
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.routes.js';
 import AuthRouter from './routes/auth.routes.js';
-
+import cors from 'cors';
 
 // initialization of imported library
 dotenv.config();
 const app = express();
+app.use(cors());
 
-//conneting MongoDB
+// using json as req body - should be placed before route declarations
+app.use(express.json());
+
+// connecting MongoDB
 mongoose.connect(process.env.MONGO)
-.then(()=>{console.log("Monoose conneted")})
-.catch((err)=>{console.log("error is "+err)})
-
+  .then(() => { console.log("Mongoose connected"); })
+  .catch((err) => { console.log("Error is " + err); });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// route declarations
+app.use('/api/user', UserRouter);
+app.use('/api/auth', AuthRouter);
 
-//listening app at port 3000
-const port = 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
-
-//using json as req body
-app.use(express.json());
-
-app.use('/api/user',UserRouter);
-app.use('/api/auth',AuthRouter);
-
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+// error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
 });
+
+// listening app at port 3000
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,18 +1,89 @@
-import { Link } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function signup() {
+export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    console.log(formData)
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <>
-        <div className="mx-auto max-w-2xl p-5">
+      <div className="mx-auto max-w-2xl p-5">
         <h1 className="text-3xl font-medium text-center my-5">Sign Up</h1>
-        <form className="flex flex-col gap-3">
-            <input className="rounded-lg p-3" placeholder="Username" type="text" name="userName" id="UserName" />
-            <input className="rounded-lg p-3" placeholder="Email" type="email"  name="email" id="email" />
-            <input className="rounded-lg p-3" placeholder="Password" type="password" name="password" id="password" />
-            <button className="rounded-lg my-5 bg-slate-500 p-3 text-white uppercase font-medium hover:bg-slate-400" type="submit">Sign Up</button>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={handleChange}
+            className="focus:outline-none rounded-lg p-3"
+            placeholder="Username"
+            type="text"
+            name="userName"
+            id="userName"
+          />
+          <input
+            onChange={handleChange}
+            className="focus:outline-none rounded-lg p-3"
+            placeholder="Email"
+            type="email"
+            name="email"
+            id="email"
+          />
+          <input
+            onChange={handleChange}
+            className="focus:outline-none rounded-lg p-3"
+            placeholder="Password"
+            type="password"
+            name="password"
+            id="password"
+          />
+          <button
+            className="disabled:opacity-80 rounded-lg my-5 bg-slate-500 p-3 text-white uppercase font-medium hover:bg-slate-400"
+            type="submit"
+          >
+            Sign Up
+          </button>
         </form>
-        <span>Already had an account? <Link className="text-sky-600 font-medium" to="/sign-in">Sign In</Link></span>
-        </div>
+        <span>
+          Already had an account?{" "}
+          <Link className="text-sky-600 font-medium" to="/sign-in">
+            Sign In
+          </Link>
+        </span>
+        {error && <p className="text-red-600 mt-5">{error}</p>}
+      </div>
     </>
-  )
+  );
 }
