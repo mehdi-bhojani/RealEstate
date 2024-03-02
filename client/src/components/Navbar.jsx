ab69ebf76e707e0cacc0ff8d0c38978632f512a8
mehdi-bhojani <m.mehdibhojani1@gmail.com>
2024-03-02 18:11:44 +0500

    complete Header Search Form Functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,34 @@
 // import  { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const { currentUser } = useSelector((state) => state.user);
+
   // const [menuOpen, setMenuOpen] = useState(false);
 
   // const onToggleMenu = () => {
   //   setMenuOpen(!menuOpen);
   // };
+const navigate = useNavigate();
+const handleSubmitSearch = (e) => {
+  e.preventDefault();
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.set('searchTerm',searchTerm);
+  const searchQuery = urlParams.toString();
+  navigate(`/search?${searchQuery}`)
+};
+
+useEffect(()=>{
+  const urlParams = new URLSearchParams(location.search);
+  const searchTermFomUrl = urlParams.get('searchTerm');
+  if(searchTermFomUrl){
+    setSearchTerm(searchTermFomUrl);
+  }
+},[location.search]);
 
   return (
     <div className="w-full bg-[#f2f4ff]">
@@ -21,13 +40,17 @@ const Navbar = () => {
               <span className="text-slate-700">Estate</span>
             </Link>
           </h1>
-          <form className="flex items-center bg-slate-300 rounded-sm px-2 sm:px-5 py-1 mx-auto w-fit">
+          <form onSubmit={handleSubmitSearch} className="flex items-center bg-slate-300 rounded-sm px-2 sm:px-5 py-1 mx-auto w-fit">
             <input
               type="text"
               placeholder="Search"
               className="w-[120px] bg-transparent text-white focus:outline-none px-2 py-1 sm:w-[300px]"
+              value={searchTerm}
+              onChange={(e)=> setSearchTerm(e.target.value)}
             />
-            <FaSearch className="text-white"></FaSearch>
+            <button>
+              <FaSearch className="text-white"></FaSearch>
+            </button> 
           </form>
           <ul className="flex gap-4 mx-auto sm:gap-6 align-middle">
             <li className="hover:underline hidden sm:block">
